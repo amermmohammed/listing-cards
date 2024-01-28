ccbb74dac0803dfcc457c917539f384ed0c608fa
Amer Malik Mohammed <amermalikmohammed@gmail.com>
2024-01-28 12:13:58 +0100

    added submit and cancel methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import ProductCard from "./components/ProductCard.tsx";
 import {formInputsList, productList} from "./data";
-import {useState, ChangeEvent} from "react";
+import {useState, ChangeEvent, FormEvent} from "react";
 import CustomModal from "./components/ui/Modal.tsx";
 import Button from "./components/ui/Button.tsx";
 import Input from "./components/ui/Input.tsx";
 import { IProduct } from "./interfaces";
 
 const App = () => {
-    /*State*/
-    const [product, setProduct] = useState<IProduct>({
+    const defaultProductObj = {
         title: "",
         description: "",
         price: "",
@@ -18,7 +17,11 @@ const App = () => {
             imageURL: ""
         },
         colors: [],
-    });
+    }
+    /*State*/
+    const [product, setProduct] = useState<IProduct>(
+        defaultProductObj
+    );
     const [isOpen, setIsOpen] = useState(false)
 
     /*Handler*/
@@ -38,13 +41,21 @@ const App = () => {
         })
     }
 
+    const onCancel = () => {
+        setProduct(defaultProductObj)
+        closeModal()
+    }
+    const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(product);
+    }
     /*Render*/
     const renderProductList = productList.map((product) =>
         <ProductCard key={product.id}
                      product={product}
         />);
     const renderFormInputList = formInputsList.map((input) =>
-        <div key={input.name} className="flex flex-col gap-2">
+        <div key={input.id} className="flex flex-col gap-2">
             <label htmlFor={input.id} className="mb-[1px] text-sm font-medium text-gray-700">{input.label}</label>
             <Input type={input.type} name={input.name} id={input.name} value={product[input.name]} onChange={onChangeHandler}/>
         </div>
@@ -52,7 +63,6 @@ const App = () => {
     return (
         <main className="container">
             <Button className="bg-indigo-700 hover:bg-indigo-800" onClick={openModal}>Add new Product</Button>
-
             <div
                 className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
                 gap-4 border-2 border-amber-950 m-5 p-2 rounded-md"
@@ -61,11 +71,11 @@ const App = () => {
             </div>
             <CustomModal isOpen={isOpen} closeModal={closeModal} title="Add new Product">
 
-                <form className="space-y-3">
+                <form className="space-y-3" onSubmit={submitHandler}>
                     {renderFormInputList}
                     <div className="flex items-center gap-2">
-                        <Button className="bg-indigo-700 hover:bg-indigo-800" onClick={closeModal}>Submit</Button>
-                        <Button className="bg-gray-400 hover:bg-gray-500" onClick={closeModal}>Cancel</Button>
+                        <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
+                        <Button className="bg-gray-400 hover:bg-gray-500" onClick={onCancel}>Cancel</Button>
                     </div>
                 </form>
 
